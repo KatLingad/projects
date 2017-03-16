9186d4cd9c086de133baf225697292ae54e61666
Katherine Lingad <kat.lingad14@gmail.com>
2017-03-16 07:26:12 +0000

    Initial commit

---
+++ bootcampLabs/week5Labs/jQuerySelectors/jquerySelectors.js
@@ -0,0 +1,7 @@
+// JavaScript File
+
+$("div").css("background-color", "purple");
+$(".highlight").css("width", "200px");
+$("#third").css("border", "5px solid orange");
+$("div").first().css("color", "pink");
+      
